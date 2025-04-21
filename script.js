fe4ae243d9d6db459ad2161b1617a3c28194fc05
Caue Silva <86086370+cauesilva1@users.noreply.github.com>
2025-04-21 11:33:04 -0400

    feat: adicionado modal no showcase

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-  const toggle = document.getElementById('menu-toggle');
-  const nav = document.querySelector('nav');
-
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('show');
-  });
-</script>
